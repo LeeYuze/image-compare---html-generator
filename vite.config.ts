01041c8488d72b4cf3bc1005fc9275d740f19393
LeeYuze <qw110288qw@vip.qq.com>
2026-01-29 20:42:52 +0800

    fix：域名白名单

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      preview: {
+        host: '0.0.0.0',
+        port: 4173,
+        strictPort: false,
+        allowedHosts: true
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
